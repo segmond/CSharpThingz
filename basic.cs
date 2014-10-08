f6c22b180a38e4a10bc4297ec44f8c9dccc75e41
Uzo AtHome <segmond@gmail.com>
2014-10-07 22:23:04 -0400

    more fun with csharp, bah, just a modern C. :-)

--- basic.cs
+++ basic.cs
@@ -1,5 +1,7 @@
+//#define DEBUG
 using System;
- 
+using System.Numerics;
+using System.Text;
 namespace Hello
 {
     public class HelloWorld
@@ -20,6 +22,42 @@ namespace Hello
             Console.WriteLine("bar debugging thingz");
         }
 
+        static void check_op_test() 
+        {
+            int a, b, c;
+            a = 2147483647;
+            b = 3;
+            c = a + b; // this won't throw an exception and silently overflow
+            //c = checked(a + b); // this will throw an exception
+            Console.WriteLine("c is " + c);
+            //OverflowException;
+        }
+
+        static void big_int_test()
+        {
+            BigInteger i = 1;
+            BigInteger j = 1;
+            int count = 0;
+            while (true) {
+                if (count++ % 100000 == 0) {
+                    Console.WriteLine(j);
+                }
+                BigInteger next = i+j;
+                i = j;
+                j = next;
+            }
+        }
+
+        static void string_test(string foo)
+        {
+            foo = foo.ToUpper();
+        }
+
+        static void stringbuilder_test(StringBuilder foo)
+        {
+            foo = foo.Replace(foo.ToString(), foo.ToString().ToUpper());
+        }
+
         static public void Main ()
         {
             string user = "Seggy";
@@ -29,6 +67,14 @@ namespace Hello
             }
             foo("FooLabel > ");
             bar("Barzz");
+            check_op_test();
+            //big_int_test();
+            string_test(user);
+            Console.WriteLine(user);
+
+            StringBuilder s = new StringBuilder("Ygges");
+            stringbuilder_test(s);
+            Console.WriteLine(s);
         }
     }
 }
